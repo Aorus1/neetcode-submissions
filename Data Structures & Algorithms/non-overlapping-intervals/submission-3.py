0302a00e4f4a62e7bc231b0e3979328c1f883bb6
Maya <56408670+Aorus1@users.noreply.github.com>
2026-08-22 16:18:53 -0700

    Add: non-overlapping-intervals - submission-3

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0], x[1]))
+        count = 0
+
+        res = []
+
+        print(intervals)
+
+        for interval in intervals:
+            if not res or res[-1][1] <= interval[0]:
+                res.append(interval)
+                continue
+                
+            elif res[-1][1] > interval[0]:
+                if (interval[1] < res[-1][1]):
+                    res[-1] = interval
+                count += 1
+                continue
+
+        return count
+
+            
+
