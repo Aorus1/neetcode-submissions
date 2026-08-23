69aa37570f99bebecf1fe6b78d7176fcc6f5b465
Maya <56408670+Aorus1@users.noreply.github.com>
2026-08-23 13:52:46 -0700

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        
+        neighbors = {i: [] for i in range(n+1)}
+        dist = [float("inf")] * (n+1)
+        prev = [-1] * (n+1)
+
+
+
+        for source, target, delay in times:
+            neighbors[source].append((target, delay))
+            
+        q = []
+        for i in range(1, n+1):
+            heapq.heappush(q, (dist[i], i))
+
+        dist[k] = 0
+        dist[0] = 0
+        
+        while q:
+            _, u = heapq.heappop(q)
+            for v, delay in neighbors[u]:
+                temp = dist[u] + delay
+                if temp < dist[v]:
+                    dist[v] = temp
+                    prev[v] = u
+                    heapq.heappush(q, (dist[v], v))
+        if float("inf") in dist:
+            return -1
+        return max(dist)
+
+        
