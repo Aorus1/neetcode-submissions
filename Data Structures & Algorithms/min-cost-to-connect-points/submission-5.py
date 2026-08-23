af115db0afd85a49815253d220c9bc87bae366d3
Maya <56408670+Aorus1@users.noreply.github.com>
2026-08-23 16:42:04 -0700

    Add: min-cost-to-connect-points - submission-5

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        visited = set()
+        total = 0
+
+        min_heap = [(0, 0, None)]
+
+        def dist(p1, p2):
+            xi, yi = p1
+            xj, yj = p2
+            return abs(xi - xj) + abs(yi-yj)
+
+        while min_heap:
+            weight, current_node, parent = heapq.heappop(min_heap)
+
+            if current_node in visited:
+                continue
+
+            visited.add(current_node)
+            total += weight
+            for neighbor in range(n):
+                if neighbor not in visited:
+                    heapq.heappush(min_heap, (dist(points[neighbor], points[current_node]), neighbor, current_node))
+        return total
+        
