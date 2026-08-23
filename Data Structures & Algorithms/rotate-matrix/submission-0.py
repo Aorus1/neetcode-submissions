c909175aa6d09452fdf01055248f62d392e9e831
Maya <56408670+Aorus1@users.noreply.github.com>
2026-08-22 17:17:04 -0700

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        matrix.reverse()
+        for i in range(n):
+            for j in range(i, n):
+                if i != j:
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        return
+
+        
