8d80dae59030129acd636cf8e24a057b32202dc3
Maya <56408670+Aorus1@users.noreply.github.com>
2026-08-22 18:50:47 -0700

    Add: max-area-of-island - submission-8

---
+++ Data Structures & Algorithms/max-area-of-island/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        maxarea = 0
+
+        visited = set()
+
+
+        def dfs(i, j):
+            if (i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != 1):
+                return 0
+            area = 1
+            grid[i][j] = 0
+            for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                area += dfs(i + di, j + dj)
+            return area
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    area = dfs(i, j)
+                    maxarea = max(area, maxarea)
+        
+        
+        return(maxarea)
+
+            
+            
