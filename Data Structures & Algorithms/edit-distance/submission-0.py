efb7e197e59a502676261e346fa2b1a12bcddb70
Maya <56408670+Aorus1@users.noreply.github.com>
2026-08-24 13:20:25 -0700

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.py
@@ -0,0 +1,20 @@
+from functools import cache
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        
+        @cache
+        def dp(i, j):
+            if i == len(word1):
+                return len(word2) - j
+            if j == len(word2):
+                return len(word1) - i
+            if word1[i] == word2[j]:
+                return dp(i+1, j+1)
+            
+            ins_op = dp(i, j+1)
+            del_op = dp(i+1, j)
+            rep_op = dp(i+1, j+1)
+
+            return 1 + min(ins_op, del_op, rep_op)
+        return dp(0, 0)
+            
